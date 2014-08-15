4bc31bda4a0f7aea0d67ae1bc4fffdc409b1abde
Michael Angerman <stormasm@gmail.com>
2014-08-15 08:27:49 -0700

    save with the atom editor no changes for re-format

--- lib/rabbitmq.rb
+++ lib/rabbitmq.rb
@@ -4,34 +4,34 @@ class RabbitMQ
 
   LOG = Logger.new(STDOUT)
   TAG = "[RabbitMQ]"
-  
+
   @@conn_string = nil
-  
+
   CONNECTION_SETTINGS = {
-    :on_tcp_connection_failure => Proc.new do |settings| 
+    :on_tcp_connection_failure => Proc.new do |settings|
       LOG.error "#{TAG} TCP Connection Failure."
-      puts "TCP Connection Failure. Retrying in 10 seconds..." 
+      puts "TCP Connection Failure. Retrying in 10 seconds..."
     end,
     :on_possible_authentication_failure => Proc.new do |settings|
       LOG.error "Authentication failed. Settings are: #{settings.inspect}"
       puts "Authentication failed. Settings are: #{settings.inspect}"
     end
   }
-  
+
   def self.setup(settings = {})
     if settings
-      @@conn_string = "amqp://#{settings['user']}@#{settings['host']}:#{settings['port']}"    
+      @@conn_string = "amqp://#{settings['user']}@#{settings['host']}:#{settings['port']}"
     end
   end
-  
+
   def self.publish_message_visitor(message_hash)
     publish_message message_hash, 'test.spnee.visitors'
   end
-  
+
   def self.publish_message_visit(message_hash)
     publish_message message_hash, 'test.spnee.visits'
   end
-  
+
   def self.publish_test
     time = Time.now.utc
     start = Time.parse("2014-01-23 00:00 UTC")
@@ -42,22 +42,22 @@ class RabbitMQ
   end
 
   def self.publish_test_fanout_message
-    message_hash = {} 
+    message_hash = {}
     message_hash[:created_at] = Time.now
     message_hash[:payload] = "test fanout"
     publish_message message_hash, 'test.spnee.messages.fanout'
   end
 
   def self.publish_test_direct_message
-    message_hash = {} 
+    message_hash = {}
     message_hash[:created_at] = Time.now
     message_hash[:payload] = "test direct"
 
     if @@conn_string.nil?
-      LOG.debug("#{TAG} Connection String is nil.")    
+      LOG.debug("#{TAG} Connection String is nil.")
     end
     unless @@conn_string.nil?
-      #LOG.debug("#{TAG} Entering #{__method__}...")    
+      #LOG.debug("#{TAG} Entering #{__method__}...")
       amqp do |connection|
         AMQP::Channel.new(connection) do |channel|
           #LOG.debug("#{TAG} Channel created")
@@ -68,7 +68,7 @@ class RabbitMQ
 
           exchange = channel.direct("test.spnee.messages.direct", :passive => true)
 
-          #LOG.debug("#{TAG} [request] Sending a message to #{exchange_name}...") 
+          #LOG.debug("#{TAG} [request] Sending a message to #{exchange_name}...")
           exchange.publish(message_hash.to_json,
                         :mandatory => true,
                         :persistent => true,
@@ -79,15 +79,15 @@ class RabbitMQ
       end
     end
   end
-  # Push a message to a RabbitMQ exchange. 
-  # Message should be a hash so it can be converted to JSON. 
+  # Push a message to a RabbitMQ exchange.
+  # Message should be a hash so it can be converted to JSON.
   # Exchange name is also declared in method call.
   def self.publish_message(message_hash, exchange_name)
     if @@conn_string.nil?
-      LOG.debug("#{TAG} Connection String is nil.")    
+      LOG.debug("#{TAG} Connection String is nil.")
     end
     unless @@conn_string.nil?
-      #LOG.debug("#{TAG} Entering #{__method__}...")    
+      #LOG.debug("#{TAG} Entering #{__method__}...")
       amqp do |connection|
         AMQP::Channel.new(connection) do |channel|
           #LOG.debug("#{TAG} Channel created")
@@ -100,11 +100,11 @@ class RabbitMQ
           #if publisher_index % 1000 == 0
           #  puts "[#{TAG} - #{time}]: publisher_index = #{publisher_index.to_s}"
           #end
-          # Exchange is a "fanout", meaning all queues bound to it 
+          # Exchange is a "fanout", meaning all queues bound to it
           # will have the message pushed to them. Passive tells the server
           # not to create a new exchange if it already exists.
           exchange = channel.fanout(exchange_name, :passive => true)
-          #LOG.debug("#{TAG} [request] Sending a message to #{exchange_name}...") 
+          #LOG.debug("#{TAG} [request] Sending a message to #{exchange_name}...")
           exchange.publish(message_hash.to_json,
                         :mandatory => true,
                         :persistent => true,
@@ -113,7 +113,7 @@ class RabbitMQ
         end
       end
     end
-  end 
+  end
 
   #Example exchange_name => "test.spnee.visitors"
   #Example queue_name => "test.visitors.postgres"
@@ -126,21 +126,21 @@ class RabbitMQ
       # This is a durable queue, it won't be destroyed if the queue empties.
       queue = channel.queue(queue_name, :durable => true).bind(exchange)
       #LOG.debug("#{TAG} #{queue.name} is ready to go.")
-      queue.subscribe do |metadata, payload| 
+      queue.subscribe do |metadata, payload|
         visitor_hash = JSON.parse(payload)
         visitor = Visitor.new()
         visitor.visitor_id = visitor_hash["visitor_id"]
         visitor.account_id = visitor_hash["account_id"]
-        visitor.created_at = visitor_hash["created_at"] 
+        visitor.created_at = visitor_hash["created_at"]
         visitor.updated_at = visitor_hash["updated_at"]
         visitor.save
-      end 
+      end
     end
   end
 
   protected
-  
-  def self.amqp(&block)                  
+
+  def self.amqp(&block)
       #LOG.debug("#{TAG} Entering #{__method__}...")
       # On the next turn of the EM reactor...
       EventMachine.next_tick do
@@ -163,7 +163,7 @@ class RabbitMQ
             #LOG.debug("#{TAG} Connected")
             yield(AMQP.connection)
           end
-        end 
+        end
       end
   end
 
