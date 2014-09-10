bb83a6f066f8fb5fe9df22bcbc0b0b2327ab225d
Michael Angerman <stormasm@gmail.com>
2014-09-10 10:55:28 -0700

    rest01 now working with Spn.ee in anticipation of port back

--- test/rest01.rb
+++ test/rest01.rb
@@ -10,5 +10,6 @@ require 'rest_client'
 #RestClient.post "http://localhost:4567/test01", { 'x' => 1 }.to_json, :content_type => :json, :accept => :json
 
 jdata = {:key => 'Hi from Corvallis'}.to_json
-response = RestClient.post 'http://localhost:4567/generic/visit/151', :data => jdata, :content_type => :json, :accept => :json
+#response = RestClient.post 'http://localhost:4567/generic/visit/151', :data => jdata, :content_type => :json, :accept => :json
+response = RestClient.post 'http://localhost:4567/api/1.0/event/151', :data => jdata, :content_type => :json, :accept => :json
 puts response
