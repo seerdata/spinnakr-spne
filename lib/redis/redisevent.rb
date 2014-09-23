1745f832752900ef1cc6a8479283a58a9dd0d129
Michael Angerman <stormasm@gmail.com>
2014-09-23 09:35:54 -0700

    code cleanup

--- lib/redis/redisevent.rb
+++ lib/redis/redisevent.rb
@@ -1,3 +1,8 @@
+# https://github.com/stormasm/sds-warden/blob/rediswarden/doc/redisapi.md
+# Tokens map to account and project
+# Accounts map to Dbs
+# You can have multiple projects in a DB, but not multiple accounts
+
 require 'json'
 require 'redis'
 require_relative './redisoptions'
@@ -6,8 +11,6 @@ class RedisEvent
 
   def initialize
     @redisc ||= Redis.new :host => REDIS_OPTIONS['host']
-    @db_zero = 0
-    @db_account = '100'
   end
 
   def build_primary_key(project,dimension,key,primarykey)
@@ -16,14 +19,6 @@ class RedisEvent
 
   def build_set_key(project,dimension,key)
     project + ":" + dimension + ":" + key + ":set"
-  end
-
-  # https://github.com/stormasm/sds-warden/blob/rediswarden/doc/redisapi.md
-  # Tokens map to account and project
-  # Accounts map to Dbs
-  # You can have multiple projects in a DB, but not multiple accounts
-  def get_db_from_token
-    @db_account
   end
 
   def get_data(db_number,project,dimension,key)
@@ -35,25 +30,19 @@ class RedisEvent
       keyprimary = build_primary_key(project,dimension,key,primary_key)
       hmap = @redisc.hgetall(keyprimary)
       myary.push(hmap)
-      #print primary_key; puts
-      #print hmap; puts
     end
-    #puts '-----------'
-    #print myary; puts
     myary
   end
 
   def get_json(db_number,project,dimension,key)
-    #@redisc.select db_number
-    #key = build_set_key(project,dimension,key)
-    #hmap = @redisc.hgetall(key)
-    #json = JSON::generate(hmap)
     data = get_data(db_number,project,dimension,key)
     json = JSON::generate(data)
   end
 
 end
 
-#rse = RedisEvent.new
-#json = rse.get_json('100','1','job-skills','ios')
-#print json; puts
+=begin
+rse = RedisEvent.new
+json = rse.get_json('100','1','job-skills','ios')
+print json; puts
+=end
