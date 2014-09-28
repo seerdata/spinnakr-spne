1a4cf6b6c1a312c52623521e355c4d2d6b7b7e17
Michael Angerman <stormasm@gmail.com>
2014-09-28 01:41:25 -0700

    update event01 with new uuids

--- test/event01.rb
+++ test/event01.rb
@@ -1,4 +1,9 @@
 require 'rest_client'
 
-response = RestClient.get 'http://localhost:4567/api/1.0/event/1/job-skills/ios', :content_type => :json, :accept => :json, :access_token => 'aef7d66a-bf0d-4a1e-8c5f-ded77f036718'
+response = RestClient.get 'http://localhost:4567/api/1.0/event/1/job-skills/ios', :content_type => :json, :accept => :json, :access_token => '104a5866-b844-4186-9322-59cacdcec297'
+puts response
+
+puts
+
+response = RestClient.get 'http://localhost:4567/api/1.0/event/2/job-skills/ruby', :content_type => :json, :accept => :json, :access_token => '25f32255-aaeb-4d2f-8988-26494bc4d58d'
 puts response
