c85d219f92d7fedfa942ef104557a630666c6386
Michael Angerman <stormasm@gmail.com>
2014-11-05 13:31:01 -0800

    adding test data to tokentransform

--- lib/redis/tokentransform.rb
+++ lib/redis/tokentransform.rb
@@ -22,3 +22,19 @@ class Transform
     hm
   end
 end
+
+=begin
+hm = Hash.new
+#hm['access_token'] = '104a5866-b844-4186-9322-19cacdcec297'
+#hm['access_token'] = '27ffa057-1878-46ea-9450-34475347e0d2'
+hm['access_token'] = '3339efca-5e99-4ea9-9cff-2075136e04bf'
+hm['dimension'] = "job-skills"
+hm['key'] = "java"
+hm['value'] = "5.05"
+hm['created_at'] = "2014-11-06 17:34:38 -0800"
+hm['interval'] = ["hours","weeks","months"]
+hm['calculation'] = ["count","sum","average","standard_deviation","linear_regression"]
+t = Transform.new
+tmessage = t.transform_customer_token(hm)
+print tmessage; puts
+=end
