547aa254f1a43cde4ffb679c6a1cbbb5c400bcae
Michael Angerman <stormasm@gmail.com>
2014-11-10 12:14:09 -0800

    working on both admin api calls

--- lib/admin_manager.rb
+++ lib/admin_manager.rb
@@ -17,11 +17,20 @@ module AdminManager
   def handle_admin_token()
     cmessage = JSON.parse(params[:data])
     print cmessage; puts
+    rt = RedisToken.new
+    token = cmessage['access_token']
+    account = cmessage['account']
+    project = cmessage['project']
+    rt.create_uuid_account_project(token, account, project)
   end
 
   def handle_admin_account()
     cmessage = JSON.parse(params[:data])
     print cmessage; puts
+    rt = RedisToken.new
+    account = cmessage['account']
+    project = cmessage['project']
+    rt.create_uuid_from_apkey(account, project)
   end
 
 end
