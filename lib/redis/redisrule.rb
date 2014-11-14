69149a9a1fd14da5e710e1436f209fb6a018f9d0
Michael Angerman <stormasm@gmail.com>
2014-11-14 09:56:03 -0800

    starting to work on getting dbnumber

--- lib/redis/redisrule.rb
+++ lib/redis/redisrule.rb
@@ -1,4 +1,5 @@
 require 'redis'
+require_relative './redistoken'
 
 class RedisRule
 
@@ -11,15 +12,27 @@ class RedisRule
     @db_start = 100
     @key_db_next = 'nextdb'
     @key_db_mapping = 'hm:accountid:db'
+    @rt = RedisToken.new
   end
 
   def process_comparator(hmap)
-    print hmap['account']; puts
-    print hmap['operator']; puts
+    account = hmap['account']
+    project = hmap['project']
+    dbnumber = @rt.getDbNumber_from_accountid(account)
+    print 'RedisRule: processing comparator '
+    print 'account = ', account, ' project = ', project
+    print ' dbnumber = ', dbnumber; puts
+    account = hmap['account']
+    print 'operator = ', hmap['operator']; puts
   end
 
   def process_observer(hmap)
-    print hmap['account']; puts
-    print hmap['trigger']; puts
+    account = hmap['account']
+    project = hmap['project']
+    dbnumber = @rt.getDbNumber_from_accountid(account)
+    print 'RedisRule: processing observer '
+    print 'account = ', account, ' project = ', project
+    print ' dbnumber = ', dbnumber; puts
+    print 'trigger = ', hmap['trigger']; puts
   end
 end
