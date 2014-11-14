7dc9db95eb98eb82688ea8758f204b1528785888
Michael Angerman <stormasm@gmail.com>
2014-11-14 12:50:21 -0800

    working on setting the primary key

--- lib/redis/redisrule.rb
+++ lib/redis/redisrule.rb
@@ -5,34 +5,47 @@ class RedisRule
 
   def initialize
     @redisc ||= Redis.new :host => REDIS_OPTIONS['host']
-    @db_uuid = 10
-    @db_ap = 11
-    @db_dbnumber = 12
-    @db_admin = 13
-    @db_start = 100
-    @key_db_next = 'nextdb'
-    @key_db_mapping = 'hm:accountid:db'
     @rt = RedisToken.new
+    @key_primary_key = 'rules-primary-key'
+
+    #@db_uuid = 10
+    #@db_ap = 11
+    #@db_dbnumber = 12
+    #@db_admin = 13
+    #@db_start = 100
+    #@key_db_next = 'nextdb'
+    #@key_db_mapping = 'hm:accountid:db'
+  end
+
+  def get_primary_key(dbnumber)
+    @redisc.select dbnumber
+    @redisc.incr @key_primary_key
+    @redisc.get @key_primary_key
   end
 
   def process_comparator(hmap)
     account = hmap['account']
     project = hmap['project']
     dbnumber = @rt.getDbNumber_from_accountid(account)
+    primarykey = get_primary_key(dbnumber)
     print 'RedisRule: processing comparator '
     print 'account = ', account, ' project = ', project
     print ' dbnumber = ', dbnumber; puts
-    account = hmap['account']
     print 'operator = ', hmap['operator']; puts
+    print 'primary key = ', primarykey; puts
   end
 
   def process_observer(hmap)
     account = hmap['account']
     project = hmap['project']
     dbnumber = @rt.getDbNumber_from_accountid(account)
+    primarykey = get_primary_key(dbnumber)
     print 'RedisRule: processing observer '
     print 'account = ', account, ' project = ', project
     print ' dbnumber = ', dbnumber; puts
     print 'trigger = ', hmap['trigger']; puts
+    print 'primary key = ', primarykey; puts
   end
+
+
 end
