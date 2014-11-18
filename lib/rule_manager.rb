72c138c17bb7d811e78a470d5660b70b1e8526a4
Michael Angerman <stormasm@gmail.com>
2014-11-18 12:59:54 -0800

    update rules api with new POST methodology

--- lib/rule_manager.rb
+++ lib/rule_manager.rb
@@ -3,15 +3,19 @@ require_relative './redis/redisrule'
 
 module RuleManager
 
+  def get_request_data
+    request.body.rewind
+    data = JSON.parse request.body.read
+  end
+
   def handle_rule_comparator()
-    hmap = JSON.parse(params[:data])
-    #print hmap; puts
+    hmap = get_request_data
     rr = RedisRule.new
     rr.process_comparator(hmap)
   end
 
   def handle_rule_observer()
-    hmap = JSON.parse(params[:data])
+    hmap = get_request_data
     rr = RedisRule.new
     rr.process_observer(hmap)
   end
